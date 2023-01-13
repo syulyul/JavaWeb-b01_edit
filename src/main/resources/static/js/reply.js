97797b8b10191c96bab5796921ed045e5cac2028
syulyul <syul9800@gmail.com>
2023-01-13 15:18:34 +0900

    PART6 finished

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -20,3 +20,23 @@ async function getList({bno, page, size, goLast}){
 
     return result.data
 }
+
+async function addReply(replyObj) {
+    const response = await axios.post(`/replies/`,replyObj)
+    return response.data
+}
+
+async function getReply(rno) {
+    const response = await axios.get(`/replies/${rno}`)
+    return response.data
+}
+
+async function modifyReply(replyObj) {
+    const response = await  axios.put(`/replies/${replyObj.rno}`, replyObj)
+    return response.data
+}
+
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
+    return response.data
+}
